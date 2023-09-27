544d285eb9507f0d76445ef55c208297fa935ae9
AShish Patel <ashishpatel287680@gmail.com>
2023-09-27 20:47:37 +0530

    navbar done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,4 @@
 import React, { useState } from 'react';
-import Lottie from "lottie-react";
-import animationSvg from './assets/form_animation.json'
-import { useNavigate } from 'react-router-dom'
-import loadingsvg from './assets/loading.json'
 import Nav from './Nav';
 
 function Home() {
@@ -14,7 +10,6 @@ function Home() {
                 </div>
                 <div>
                     <div>
-
                         <p>Greg understood that this situation would make Michael terribly uncomfortable. Michael simply had no idea what was about to come and even though Greg could prevent it from happening, he opted to let it happen. It was quite ironic, really. It was something Greg had said he would never wish upon anyone a million times, yet here he was knowingly letting it happen to one of his best friends. He rationalized that it would ultimately make Michael a better person and that no matter how uncomfortable, everyone should experience racism at least once in their lifetime.</p>
                     </div>
                 </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import './CSS/Nav.css';
-import { LayoutDashboard, Repeat, Wallet, Settings } from 'lucide-react'
+import { LayoutDashboard, Repeat, Wallet, Settings, FileBarChart, LogOut } from 'lucide-react'
 import logo from './assets/logo-svg.svg'
+import scrimg from './assets/images-removebg-preview.png'
 import { Link } from 'react-router-dom'
 const Nav = () => {
     return (
         <header>
             <nav className='nav-container'>
                 <div className="logo-container">
-                    <img className='logo-svg' src={logo} alt='logo'></img>
+                    {/* <img className='logo-svg' src={logo} alt='logo'></img> */}
+                    <h1 className='logo-title'>Expenzify</h1>
+                    <sup>
+                        <img className='super-image' src={scrimg} alt='super script'></img>
+                    </sup>
                 </div>
                 <div className='nav-body'>
                     <div className='nav-links'>
@@ -16,45 +21,93 @@ const Nav = () => {
 
                             <li className='link-list'>
                                 <div>
-                                    <LayoutDashboard color="#f0f0f0" />
-                                </div>
-                                <div>
-                                    <Link to='/home'>Dashboard</Link>
-                                </div>
-                            </li>
-
-                            <li className='link-list'>
-                                <div>
-                                    <Repeat color="#f0f0f0" />
-                                </div>
-                                <div>
-                                    <Link to='/home'>Expense</Link>
+                                    <Link to='/home'>
+                                        <div className='link-grid'>
+                                            <span>
+                                                <LayoutDashboard color="#f0f0f0" />
+                                            </span>
+                                            <p className='link-text'>Dashboard</p>
+                                        </div>
+                                    </Link>
                                 </div>
                             </li>
 
                             <li className='link-list'>
                                 <div>
-                                    <Wallet color="#f0f0f0" />
-                                </div>
-                                <div>
-                                    <Link to='/home'>Income</Link>
+                                    <Link to='/home'>
+                                        <div className='link-grid'>
+                                            <span>
+                                                <Repeat color="#f0f0f0" />
+                                            </span>
+                                            <p className='link-text'>Expense</p>
+                                        </div>
+                                    </Link>
                                 </div>
                             </li>
 
                             <li className='link-list'>
                                 <div>
-                                    <Settings color="#f0f0f0" />
+                                    <Link to='/home'>
+                                        <div className='link-grid'>
+                                            <span>
+                                                <Wallet color="#f0f0f0" />
+                                            </span>
+                                            <p className='link-text'>Income</p>
+                                        </div>
+                                    </Link>
                                 </div>
+                            </li>
+
+                            <li className='link-list'>
                                 <div>
-                                    <Link to='/home'>Setting</Link>
+                                    <Link to='/home'>
+                                        <div className='link-grid'>
+                                            <span>
+                                                <FileBarChart color="#f0f0f0" />
+                                            </span>
+                                            <p className='link-text'>Reports</p>
+                                        </div>
+                                    </Link>
                                 </div>
                             </li>
 
                         </ul>
                     </div>
+                    <hr className='line'></hr>
                 </div>
                 <div className='nav-footer'>
+                    <div className='nav-body'>
+                        <div className='nav-links'>
+                            <ul>
+                                <li className='link-list'>
+                                    <div>
+                                        <Link to='/home'>
+                                            <div className='link-grid'>
+                                                <span>
+                                                    <Settings color="#f0f0f0" />
+                                                </span>
+                                                <p className='link-text'>Setting</p>
+                                            </div>
+                                        </Link>
+                                    </div>
+                                </li>
 
+                                <li className='link-list'>
+                                    <div>
+                                        <Link to='/home'>
+                                            <div className='link-grid'>
+                                                <span>
+                                                    <LogOut color="#f0f0f0" />
+                                                </span>
+                                                <p className='link-text'>Logout</p>
+                                            </div>
+                                        </Link>
+                                    </div>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </nav>
         </header>
