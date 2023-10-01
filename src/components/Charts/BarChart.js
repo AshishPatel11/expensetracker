656231d20f6e115fbdc86dafa957b37f76db3d9c
AShish Patel <ashishpatel287680@gmail.com>
2023-10-01 21:44:42 +0530

    chart UI done

--- src/components/Charts/BarChart.js
+++ src/components/Charts/BarChart.js
@@ -10,44 +10,52 @@ import {
 } from 'chart.js';
 
 import { Bar } from 'react-chartjs-2';
+import '../CSS/Chart.css'
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
 export default function BarChart() {
 
-    ChartJS.register(
-        CategoryScale,
-        LinearScale,
-        BarElement,
-        Title,
-        Tooltip,
-        Legend
-    );
 
     const options = {
         responsive: true,
+        maintainAspectRatio: 'true',
+        // aspectRatio: 1 | 3,
         plugins: {
             legend: {
                 position: 'top',
+                Alignment: 'start'
             },
             title: {
                 display: true,
-                text: 'Chart.js Bar Chart',
+                text: 'Expense Report',
+                color: "#006770",
+                position: "top"
             },
         },
     };
 
-    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
+    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Augest', 'September', 'October', 'November', 'December'];
     const data = {
         labels,
         datasets: [
             {
                 label: 'Dataset 1',
-                data: [10, 20, 30, 50, 60, 70, 80],
-                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                data: [10, 20, 30, 50, 60, 70, 80, 20, 60, 50, 60, 80],
+                backgroundColor: '#006770a1',
+                borderRadius: "4",
             },
             {
                 label: 'Dataset 2',
-                data: [50, 20, 30, 50, 60, 70, 80],
-                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                data: [50, 20, 30, 50, 60, 70, 80, 50, 60, 70, 90, 10],
+                backgroundColor: '#00adbca1',
+                borderRadius: "4",
             },
         ],
     };
@@ -56,7 +64,9 @@ export default function BarChart() {
     return (
 
         <>
-            <Bar options={options} data={data} />;
+            <div className='Bar-chart'>
+                <Bar options={options} data={data} />
+            </div>
         </>
     )
 }
