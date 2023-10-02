acb6cac696cd1538ac1f2054004e4b57ced2ac6e
AShish Patel <ashishpatel287680@gmail.com>
2023-10-02 13:08:39 +0530

    Expense history

--- src/components/ExpenseHistory.js
+++ src/components/ExpenseHistory.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Nav from './Nav';
+import Head from './Head';
+import { BellPlus, PlusCircle, X } from 'lucide-react'
+import LoginAuth from './LoginAuth';
+import Card from './Card';
+import NewExpense from './Expenses/NewExpense';
+import NewReminder from './Expenses/NewReminder';
+import BarChart from './Charts/BarChart';
+import PieChart from './Charts/PieChart';
+
 function ExpenseHistory() {
+
+
   return (
-    <div>
-             <div className="head-container">
-                <h1>Expence History</h1>
-             </div>
-    </div>
+    <>
+      <LoginAuth />
+      <div className='body-container'>
+        <div className="NavBar">
+          <Nav />
+        </div>
+        <div className='dash-container'>
+          <div>
+            <Head />
+          </div>
+        </div>
+      </div >
+    </>
   )
 }
 
